01c4fa946eefbbb6dc1405bfa2735d0fd4dcf6f4
Alexander <alexander@Alexanders-Mac-Pro.local>
2014-11-26 21:59:07 +0200

    Sqash migrations

--- db/migrate/20141126194488_add_default_to_spree_stock_locations.spree.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree (originally 20140717185932)
-class AddDefaultToSpreeStockLocations < ActiveRecord::Migration
-  def change
-    add_column :spree_stock_locations, :default, :boolean, null: false, default: false
-  end
-end
