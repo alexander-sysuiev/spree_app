5a88fc9b9a22e43cc1f7d5c86eae96d16eef0a09
Alexander <alexander@Alexanders-Mac-Pro.local>
2014-11-26 21:48:14 +0200

    Fix migrations for proper spree version

--- db/migrate/20141116144721_add_test_admin.rb
+++
@@ -1,7 +0,0 @@
-class AddTestAdmin < ActiveRecord::Migration
-  def change
-  	admin = Spree::User.create(email: 'admin@example.com', password: '123456')
-  	admin.spree_roles = Spree::Role.where(name: 'admin')
-  	admin.save!
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
+
+
+Spree::Core::Engine.load_seed if defined?(Spree::Core)
+Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
